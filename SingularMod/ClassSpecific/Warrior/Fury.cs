5299fc361ab9f73eb093acfdb92e1a1aebe2eaed
Kha P Nguyen <superkhung@4vn.org>
2013-05-03 18:21:19 +0700

    Fury update

--- SingularMod/ClassSpecific/Warrior/Fury.cs
+++ SingularMod/ClassSpecific/Warrior/Fury.cs
@@ -41,61 +41,33 @@ namespace Singular.ClassSpecific.Warrior
                     Spell.Cast("Rallying Cry", ret => Me.HealthPercent <= 20),
 
 				    //Bloodthurst use on CD but only use to build rage on Execute phase
-                    Spell.Cast("Bloodthirst", ret => !WithinExecuteRange || (Me.CurrentTarget.HealthPercent <= 20 && Me.RagePercent <= 30)),
-                    Spell.Cast("Colossus Smash"),
+                    Spell.Cast("Bloodthirst", ret => Me.CurrentTarget.IsWithinMeleeRange),
+                    Spell.Cast("Colossus Smash", ret => Me.CurrentTarget.IsWithinMeleeRange),
+                    Spell.Cast
+                    (
+                        "Whirlwind", ret =>
+                            //Get 3 stack Meat Cleaver for Raging Blow to hit 4 targets if more than 3 targets nearby
+                        Me.CurrentRage >= 30 && Unit.NearbyUnfriendlyUnits.Count(u => u.IsWithinMeleeRange) >= 4 && !Me.HasAura("Meat Cleaver", 3) ||
+                            //Get 2 stack Meat Cleaver for Raging Blow to hit 3 targets if 3 targets nearby
+                        Me.CurrentRage >= 30 && Unit.NearbyUnfriendlyUnits.Count(u => u.IsWithinMeleeRange) == 3 && !Me.HasAura("Meat Cleaver", 2) ||
+                            //Get 1 stack Meat Cleaver for Raging Blow to hit 2 targets if 2 targets nearby
+                        Me.CurrentRage >= 30 && Unit.NearbyUnfriendlyUnits.Count(u => u.IsWithinMeleeRange) == 2 && !Me.HasAura("Meat Cleaver")
+                    ),
 
+                    Spell.Cast("Raging Blow", ret => Me.CurrentTarget.IsWithinMeleeRange),
 				    //Handle selfbuff
                     Spell.BuffSelf("Berserker Rage", ret => !IsEnraged && Me.CurrentTarget.IsWithinMeleeRange),
-                    Spell.BuffSelf("Bloodbath"),
+                    Spell.BuffSelf("Bloodbath", ret => Me.CurrentTarget.IsWithinMeleeRange),
                     Spell.BuffSelf("Skull Banner", ret => WithinExecuteRange && Unit.IsBoss(Me.CurrentTarget)),
-                    //Spell.BuffSelf("Deadly Calm", ret => Me.RagePercent >= 40 && TargetSmashed && !WithinExecuteRange),
 				    //Only use Recklessness on Execute phase
                     Spell.BuffSelf("Recklessness", ret => WithinExecuteRange && Unit.IsBoss(Me.CurrentTarget)),
 
 				    //Spam Execute on Execute phase
                     Spell.Cast("Execute", ret => WithinExecuteRange),
                     Spell.Cast("Dragon Roar", ret => Me.CurrentTarget.IsWithinMeleeRange),
-				    //Handle AOE rotation when more than 1 target nearby within 8 yard
-                    new Decorator
-				    (
-                        ret => Unit.NearbyUnfriendlyUnits.Count(u => u.IsWithinMeleeRange) > 1 && WarriorSettings.UseWarriorAOE,
-					    //ret => Unit.NearbyUnfriendlyUnits.Count(u => u.Distance <= 8 && !u.IsPlayer) >= 2,
-                        new PrioritySelector
-					    (
-						    //Handle Meat Cleaver buff for Raging Blow AOE
-						    Spell.Cast
-						    (
-							    "Whirlwind", ret =>
-				               //Get 3 stack Meat Cleaver for Raging Blow to hit 4 targets if more than 3 targets nearby
-                                Me.CurrentRage >= 30 && Unit.NearbyUnfriendlyUnits.Count() >= 4 && !Me.HasAura("Meat Cleaver", 3) ||
-				               //Get 2 stack Meat Cleaver for Raging Blow to hit 3 targets if 3 targets nearby
-                                Me.CurrentRage >= 30 && Unit.NearbyUnfriendlyUnits.Count() == 3 && !Me.HasAura("Meat Cleaver", 2) ||
-				               //Get 1 stack Meat Cleaver for Raging Blow to hit 2 targets if 2 targets nearby
-                                Me.CurrentRage >= 30 && Unit.NearbyUnfriendlyUnits.Count() == 2 && !Me.HasAura("Meat Cleaver")
-			           	    )
-					    )
-				    ),
-
-                    Spell.Cast("Raging Blow", ret =>
-			           	    Unit.UnfriendlyUnitsNearTarget(8f).Count() >= 4 && Me.HasAura("Meat Cleaver", 3) && BTCD.TotalSeconds >= 1 ||
-                    	    Unit.UnfriendlyUnitsNearTarget(8f).Count() == 3 && Me.HasAura("Meat Cleaver", 2) && BTCD.TotalSeconds >= 1 ||
-                    	    Unit.UnfriendlyUnitsNearTarget(8f).Count() == 2 && Me.HasAura("Meat Cleaver") && BTCD.TotalSeconds >= 1 ||
-			           	    Unit.UnfriendlyUnitsNearTarget(8f).Count() == 1 && BTCD.TotalSeconds >= 1 || Me.GetAuraTimeLeft("Meat Cleaver", true).TotalSeconds <= 3 && BTCD.TotalSeconds >= 1),
 
                     Spell.Cast("Heroic Strike", ret => NeedHeroicStrike),
                     Spell.Cast("Wild Strike", ret => !WithinExecuteRange && Me.HasAura("Bloodsurge")),
-				    //Ignore Wild Strike if there's more than 2 target nearby, we need to save rage for Whirlwind
-				    new Decorator
-				    (
-					    ret => Unit.NearbyUnfriendlyUnits.Count() == 1 || !WarriorSettings.UseWarriorAOE,
-					    new PrioritySelector
-					    (
-		                    Spell.Cast("Wild Strike", ret => !WithinExecuteRange && TargetSmashed && BTCD.TotalSeconds >= 1 && Me.RagePercent >= 40),
-						    Spell.Cast("Wild Strike", ret => !WithinExecuteRange && BTCD.TotalSeconds >= 1 && CSCD.TotalSeconds >= 1.6 && Me.CurrentRage >= 60)
-					    )
-				    ),
-                
-                    //Spell.Cast(Common.SelectedShout, ret => !TargetSmashed && Me.CurrentRage < 70),
                     Spell.Cast("Battle Shout", ret => Me.CurrentRage < 60)
                 )
         	);
